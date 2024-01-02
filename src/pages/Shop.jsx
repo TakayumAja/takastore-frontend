06f1279828ca7d128ff8c1d46714897cf9c8f488
TakayumAja <asraftakayuma76@gmail.com>
2024-01-02 21:04:55 +0700

    style(shop): change size paragraph and title shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,14 +5,14 @@ const Shop = () => {
   return (
     <section className="py-10">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center justify-center gap-6 w-full h-[392px] bg-[url('/Banner1.png')] bg-no-repeat bg-cover rounded-lg overflow-hidden">
+        <div className="flex flex-col items-center justify-center gap-4 md:gap-6 w-full h-[308px] md:h-[392px] bg-[url('/Banner1.png')] bg-no-repeat bg-cover rounded-lg overflow-hidden">
           <p className="flex gap-2 items-center">
             <span className="">Home</span>
             <IoIosArrowForward className="h-4 w-4" />
             <span>Shop</span>
           </p>
-          <h1 className="text-5xl font-medium">Shop Page</h1>
-          <p className="text-lg text-gray-700">
+          <h1 className="md:text-5xl text-3xl font-medium">Shop Page</h1>
+          <p className="text-base md:text-lg text-gray-700 text-center">
             Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
             cillum sint consectetur cupidatat.
           </p>
