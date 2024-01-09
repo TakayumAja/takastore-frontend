e271a41da5e8b8e5a5e59af1d85f2036e1088190
TakayumAja <asraftakayuma76@gmail.com>
2024-01-09 16:42:36 +0700

    style(login): add logo

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Logo } from "../assets";
 
 const SignIn = () => {
   return (
     <section className="flex flex-col lg:flex-row flex-1">
       <div className="basis-1/2 grow">
-        <div className="h-[40vh] lg:h-full w-full bg-no-repeat bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1517705008128-361805f42e86?q=80&w=1987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"></div>
+        <div className="relative h-[40vh] lg:h-full w-full bg-no-repeat bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1517705008128-361805f42e86?q=80&w=1987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+          <img
+            src={Logo}
+            className="absolute top-5 left-1/2 -translate-x-1/2"
+          />
+        </div>
       </div>
       <div className="basis-1/2 my-10 lg:my-0 flex items-center justify-center">
         <div className="w-[456px]">
           <form className="space-y-8">
             <div className="space-y-6">
               <h1 className="font-semibold text-[#141718] text-4xl leading-10 tracking-tight">
-                Sign Up
+                Sign In
               </h1>
               <p className="text-base font-normal text-[#6C7275] leading-6">
                 Don’t have an accout yet?{" "}
